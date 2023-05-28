4b907b679a68b76029fd295c287760d980c1d077
ElanPB <elanparkbernstein@berkeley.edu>
2023-05-27 23:13:51 -0700

    Fixed parser, should now work with spaces in author names

--- Code/endfParse.py
+++ Code/endfParse.py
@@ -2,14 +2,13 @@ import pandas as pd
 
 def parse(isotope):
     file = open('../RawData/' + isotope + '.txt', 'r')
-    lines = file.readlines()
+    lines = file.read().split('\n')
     reactions = []
     name = ''
     data = []
     col = []
     for i, e in enumerate(lines):
         if e.startswith('//'):
-            print(data[0])
             df = pd.DataFrame(data, columns=col)
             reactions.append([name, df])
             name = ''
@@ -18,23 +17,30 @@ def parse(isotope):
         elif e.startswith('#'):
             if e.startswith('#name:'):
                 name = isotope + ' (' + e.split('(')[1].split(')')[0] + ')'
-            elif (i < len(lines) - 1 and lines[i + 1][0] != '#'):
+            elif (i < len(lines) - 1 and len(lines[i + 1]) != 0 and lines[i + 1][0] != '#'):
                 col = strToArray(e)
                 for j, x in enumerate(strToArray(lines[i - 1])):
                     col[j] = x + '(' + col[j] + ')'
         else:
             line = strToArray(e)
             info = []
-            for i in range(len(line)):
+            for x in range(len(line)):
                 try:
-                    info.append(float(line[i]))
+                    info.append(float(line[x]))
                 except ValueError:
-                    info.append(line[i])
-            data.append(info)
-    print(len(reactions))
-    for i in range(min(5,len(reactions))):
-        print(i)
-        reactions[i][1].style
+                    info.append(line[x])
+            x = 0
+            while x < len(info):
+                if type(info[x]) == str:
+                    y = x + 1
+                    while (y < len(info) and type(info[y]) == str):
+                        info[x] = info[x] + ' ' + info[y]
+                        del info[y]
+                x += 1
+
+            data.append(info[:len(col)])
+    for i in range(len(reactions)):
+        print(reactions[i][1])
 
 def strToArray(text):
     text = text.replace(',', ' ')
